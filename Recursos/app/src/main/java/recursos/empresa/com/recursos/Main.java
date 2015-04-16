ef6232fa16685372b454592dfc1a94aa154e1e7b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-04-16 18:12:11 +0200

    Updated image

--- Recursos/app/src/main/java/recursos/empresa/com/recursos/Main.java
+++ Recursos/app/src/main/java/recursos/empresa/com/recursos/Main.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class Main extends ActionBarActivity {
@@ -12,6 +13,9 @@ public class Main extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.perrito);
     }
 
 
