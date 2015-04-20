7e3dd0813655d2306e801cccdc26e9d826bb6288
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-04-20 20:11:27 +0200

    Improved Lists project

--- Listas/app/src/main/java/com/empresa/listas/Main.java
+++ Listas/app/src/main/java/com/empresa/listas/Main.java
@@ -1,39 +1,60 @@
 package com.empresa.listas;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-public class Main extends ActionBarActivity {
+public class Main extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListAdapter(new MiArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                R.id.textView,
+                getResources().getStringArray(R.array.countries)
+        ));
     }
 
+    private class MiArrayAdapter extends ArrayAdapter<String> {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public MiArrayAdapter(Context context, int resource, int textViewResourceId, String[] objects) {
+            super(context, resource, textViewResourceId, objects);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater)
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = inflater.inflate(R.layout.list_items, parent, false);
+            String[] items = getResources().getStringArray(R.array.countries);
+            ImageView image = (ImageView) row.findViewById(R.id.imageView);
+            TextView text = (TextView) row.findViewById(R.id.textView);
+
+            text.setText(items[position]);
+            if ("EEUU".equals(items[position])) {
+                image.setImageResource(R.drawable.usa);
+            } else if ("Spain".equals(items[position])) {
+                image.setImageResource(R.drawable.spain);
+            } else if ("France".equals(items[position])) {
+                image.setImageResource(R.drawable.france);
+            } else if ("Japan".equals(items[position])) {
+                image.setImageResource(R.drawable.japan);
+            } else if ("Germany".equals(items[position])) {
+                image.setImageResource(R.drawable.germany);
+            }
+
+            return row;
+        }
     }
 }
