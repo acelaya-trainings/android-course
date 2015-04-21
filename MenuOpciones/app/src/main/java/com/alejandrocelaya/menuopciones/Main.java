3a752817888074259050fc385a2a42ab3a739119
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-04-21 20:09:51 +0200

    Created MenuOpciones project

---
+++ MenuOpciones/app/src/main/java/com/alejandrocelaya/menuopciones/Main.java
@@ -0,0 +1,38 @@
+package com.alejandrocelaya.menuopciones;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Main extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mimenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.item1) {
+            Log.d("Opción", "Has pulsado llamada");
+        } else if (id == R.id.item2) {
+            Log.d("Opción", "Has pulsado foto");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
