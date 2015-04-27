7ac1f364ca900b62f4e61984a3fa34741ea47a8d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-04-27 19:06:50 +0200

    Created Fragmentos project

---
+++ Fragmentos/app/src/main/java/com/alejandrocelaya/fragmentos/Fragmento1.java
@@ -0,0 +1,18 @@
+package com.alejandrocelaya.fragmentos;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cta on 27/04/2015.
+ */
+public class Fragmento1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
---
+++ Fragmentos/app/src/main/java/com/alejandrocelaya/fragmentos/Fragmento2.java
@@ -0,0 +1,17 @@
+package com.alejandrocelaya.fragmentos;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cta on 27/04/2015.
+ */
+public class Fragmento2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+}
