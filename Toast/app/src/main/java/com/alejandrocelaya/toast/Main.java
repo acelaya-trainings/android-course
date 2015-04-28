7db8e6c3befb77655a7cab8a408707a501f46aba
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-04-28 17:50:34 +0200

    Created project Toast

---
+++ Toast/app/src/main/java/com/alejandrocelaya/toast/Main.java
@@ -0,0 +1,31 @@
+package com.alejandrocelaya.toast;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Main extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(Main.this, "Foo Bar", Toast.LENGTH_LONG);
+                t.setGravity(Gravity.CENTER, 0, 0);
+                t.show();
+            }
+        });
+    }
+
+}
