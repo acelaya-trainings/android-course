fa2dc1fd0d714aba214c14dfc6c074b344a4788c
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-05-06 19:07:34 +0200

    Created Widget project

---
+++ Widget/app/src/main/java/com/widget/MiWidget.java
@@ -0,0 +1,15 @@
+package com.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MiWidget extends AppWidgetProvider {
+	@Override
+    public void onUpdate(Context context,
+                 AppWidgetManager appWidgetManager,
+                 int[] appWidgetIds) {
+        //Actualizar el widget
+        //...
+    }
+}
